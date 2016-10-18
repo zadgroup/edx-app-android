fb646acc1a5ef695ce4165dbb92a7ce03bc0fadf
Ibraheem Zaman <ibraheem@edx.org>
2016-10-18 14:41:25 +0500

    Add error notification interface & implementation
    This commit adds a generic interface for all the error notification
    types, which could then be provided to the error handler, as appropriate
    to the query type.
    
    The content error layout has also been updated to contain an action
    button, and to set it's visibility to gone by default so that it's not
    unnecessarily set up as part of the view hierarchy layout (the
    visibility of the message text view, on the other hand, has been set to
    visible).
    
    The AlertDialogFragment has also been updated to include a new static
    constructor that takes string resource IDs instead of the actual
    resolved strings, in order to not have an unneeded dependency on the
    Context in it's notification implementation.

--- OpenEdXMobile/src/main/java/org/edx/mobile/view/dialog/AlertDialogFragment.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/dialog/AlertDialogFragment.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 
@@ -21,7 +22,9 @@ import roboguice.fragment.RoboDialogFragment;
 
 public class AlertDialogFragment extends RoboDialogFragment {
     protected static final String ARG_TITLE = "title";
+    protected static final String ARG_TITLE_RES = "titleRes";
     protected static final String ARG_MESSAGE = "message";
+    protected static final String ARG_MESSAGE_RES = "messageRes";
 
     public interface ButtonAttributes {
         @NonNull
@@ -29,6 +32,16 @@ public class AlertDialogFragment extends RoboDialogFragment {
 
         @Nullable
         DialogInterface.OnClickListener getOnClickListener();
+    }
+
+    public static AlertDialogFragment newInstance(@StringRes int titleResId, @StringRes int messageResId, @Nullable DialogInterface.OnClickListener onPositiveClick) {
+        AlertDialogFragment fragment = new AlertDialogFragment();
+        fragment.mOnPositiveClick = onPositiveClick;
+        Bundle args = new Bundle();
+        args.putInt(ARG_TITLE_RES, titleResId);
+        args.putInt(ARG_MESSAGE_RES, messageResId);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     public static AlertDialogFragment newInstance(@Nullable String title, @NonNull String message, @Nullable DialogInterface.OnClickListener onPositiveClick) {
@@ -47,8 +60,13 @@ public class AlertDialogFragment extends RoboDialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        String title = getArguments().getString(ARG_TITLE);
-        String message = getArguments().getString(ARG_MESSAGE);
+        Bundle args = getArguments();
+        int titleResId = args.getInt(ARG_TITLE_RES);
+        int messageResId = args.getInt(ARG_MESSAGE_RES);
+        CharSequence title = titleResId != 0 ?
+                getText(titleResId) : args.getString(ARG_TITLE);
+        CharSequence message = messageResId != 0 ?
+                getText(messageResId) : args.getString(ARG_MESSAGE);
 
         ButtonAttributes positiveButtonAttributes = getPositiveButtonAttributes();
         ButtonAttributes negativeButtonAttributes = getNegativeButtonAttributes();
